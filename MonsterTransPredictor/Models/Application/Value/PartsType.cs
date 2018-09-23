80e6cdb221c93a5c4f311273c2e0b723402cf48a
eckolo <eckolo.0722@gmail.com>
2018-09-23 12:36:46 +0900

    スキルの部位分類を一通り追加

--- MonsterTransPredictor/Models/Application/Value/PartsType.cs
+++ MonsterTransPredictor/Models/Application/Value/PartsType.cs
@@ -5,6 +5,29 @@
     /// </summary>
     public enum PartsType
     {
-
+        /// <summary>
+        /// 頭部
+        /// </summary>
+        head,
+        /// <summary>
+        /// 腕部
+        /// </summary>
+        arm,
+        /// <summary>
+        /// 脚部
+        /// </summary>
+        leg,
+        /// <summary>
+        /// 身体
+        /// </summary>
+        body,
+        /// <summary>
+        /// ブレス
+        /// </summary>
+        breath,
+        /// <summary>
+        /// 魔法能力
+        /// </summary>
+        magic
     }
 }
