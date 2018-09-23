41f115ba59d4274fee3d414dbbe737a8d91f83dd
eckolo <eckolo.0722@gmail.com>
2018-09-23 19:47:12 +0900

    それっぽい（？）メインメニュー画面をでっちあげる

--- MonsterTransPredictor/App_Start/RouteConfig.cs
+++ MonsterTransPredictor/App_Start/RouteConfig.cs
@@ -15,8 +15,8 @@ namespace MonsterTransPredictor
 
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                url: "{controller}/{action}",
+                defaults: new { controller = "Predict", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ MonsterTransPredictor/Controllers/PredictController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MonsterTransPredictor.Controllers
+{
+    public class PredictController : Controller
+    {
+        // GET: Predict
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // GET: Predict
+        public ActionResult SkillSearch()
+        {
+            return View();
+        }
+        // GET: Predict
+        public ActionResult MonsterSearch()
+        {
+            return View();
+        }
+    }
+}
